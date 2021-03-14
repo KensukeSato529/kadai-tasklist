4738844dce43f3074b097516f5a5b5d90691c5fd
kensuke.sato <kentecacademy@gmail.com>
2021-03-14 15:51:11 +0000

    add column

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,64 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'タスクが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to tasks_url
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
+    end
+  end
+  
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id]) 
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @user
+      redirect_to user_url
+    end
+  end
+end
